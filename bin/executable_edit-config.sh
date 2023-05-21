ad29d1a0d457eb7490aeeeaa177be1e8eb9257b1
void <void@fedora.fritz.box>
2023-05-21 18:56:45 +0200

    add edit-config script

---
+++ bin/executable_edit-config.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+chdir="-c lcd %:p:h"
+#
+declare -A confedit_list
+
+confedit_list[nvim]="$HOME/.config/nvim/"
+confedit_list[tmux]="$HOME/.config/tmux/tmux.conf"
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(printf '%s\n' "${!confedit_list[@]}" | fzf)
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+# What to do when/if we choose a file to edit.
+if [ "$selected" ]; then
+    cfg=$(printf '%s\n' "${confedit_list["${selected}"]}")
+    # shellcheck disable=SC2154
+    nvim "$chdir" "$cfg"
+# What to do if we just escape without choosing anything.
+else
+    echo "Program terminated." && exit 0
+fi
