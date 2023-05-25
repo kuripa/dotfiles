a4fd18eda2355ba0acf4d3669b9b766e2d499676
kuripa <n.fuhrmann@proton.me>
2023-05-25 14:12:44 -0400

    add fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr -a -- vim nvim
+abbr -a -- vim nvim
+abbr -a -- cat bat
+abbr -a -- mv mv -v
+
+bind \cf 'tmux-sessionizer'
