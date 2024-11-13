ef8d33c1926a9bbc7a7f8b714bf785a161a6ada6
Nick Fuhrmann <n.fuhrmann@protonmail.com>
2024-11-13 04:14:21 +0100

    feat: add variables for flameshot to work

---
+++ dot_config/private_fish/conf.d/executable_profile.fish
@@ -0,0 +1,12 @@
+
+set -Ux SDL_VIDEODRIVER wayland
+set -Ux _JAVA_AWT_WM_NONREPARENTING 1
+set -Ux QT_QPA_PLATFORM wayland
+set -Ux XDG_CURRENT_DESKTOP sway
+set -Ux XDG_SESSION_DESKTOP "sway"
+
+set -Ux WLR_NO_HARDWARE_CURSORS 1
+set -Ux GDK_BACKEND wayland
+set -Ux CLUTTER_BACKEND wayland
+set -Ux SDL_VIDEODRIVER wayland
+set -Ux XDG_SESSION_TYPE wayland
