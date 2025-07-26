365dce5f17e1a91c634f8b369325730c095f519a
Nick Fuhrmann <n.fuhrmann@protonmail.com>
2025-07-26 16:00:35 +0200

    add wrapper for yazi

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,6 +7,15 @@ function fish_greeting
   fastfetch
 end 
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 if test -f /home/nfuhrman/.env 
     source /home/nfuhrman/.env
 end
